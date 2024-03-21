05c8f6ad00f92fa33bb2c449bc285b514c5ca976
Adrian Chitralla <chitralla@informatik.uni-leipzig.de>
2024-03-21 10:49:13 +0100

    overhauled common input and output folders additional changes in arguments structure

--- FastSAM_MIMUL.py
+++ FastSAM_MIMUL.py
@@ -1,6 +1,6 @@
 import os
 from fastsam import FastSAM, FastSAMPrompt
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 from utils.tools import convert_box_xywh_to_xyxy
@@ -9,18 +9,20 @@ import ast
 def get_arguments():
 
     parser = argparse.ArgumentParser(prog='MIMUL FastSAM', description='Implements FastSAM for use on MIMUL Piano Roll heads.')
+    parser.add_argument('-d', '--device', type=str,  required=False, default='cpu', help='The computing device to work on. To work on graphics card use \'CUDA\', default is \'cpu\'')
     parser.add_argument('-w', '--working_directory', type=str, required=True, help='Path to the working directory with inputs and outpus.')
     parser.add_argument('-ma', '--manufacturer', type=str, required=False, help='The piano roll manufacturer. This argument is for better sorting while testing')
-    parser.add_argument('-i', '--input_image', type=str, required=True, help='The file name of the picture (without extention) to be segmented. (Only tested with JPG.)')
-    parser.add_argument('-t', '--target', type=str, required=True, help='The target that should be segmented. This is for sorting during the test phase.')
-    parser.add_argument('-m', '--mode', type=str, required=True, help='The mode to use for manually marking the location of the label or licence stamp. For box mode type \'box\'. for points mode use \'points\'.')
+    parser.add_argument('-t', '--target', type=str, required=True, help='The object that should be segmented. This is for sorting only.')
+    parser.add_argument('-i', '--input', type=str, required=True, help='The file name of the picture (without extention) to be segmented. (Only tested with JPG.)')
+    parser.add_argument('-m', '--mode', type=str, required=True, default='box', help='The mode to use for manually marking the location of the label or licence stamp. For box mode type \'box\'. for points mode use \'points\'.')
     parser.add_argument('-b', '--box', type=str, required=False, help='The box of the label or stamp. Requires format like in FastSAM: bbox default shape [0,0,0,0] -> [x1,y1,x2,y2] Example: box = [[1692, 882, 440, 508]]')
-    parser.add_argument('-p', '--points', type=str, required=False, help='The points of the label or stamp. Requires format like in FastSAM: points default [[0,0]] [[x1,y1],[x2,y2]]')
+    parser.add_argument('-p', '--points', type=str, required=False, help='The points of the label or stamp. Requires format like in FastSAM. points default shapes: single point: [[0,0]] multiple points: [[x1,y1],[x2,y2]]')
     parser.add_argument('-pl', '--point_labels', type=str, required=False, help='The point_label to define which points belong to the foreground and which belong to the background. Requires format like in FastSAM: point_label default [0] [1,0] 0:background, 1:foreground')
+   
     args = parser.parse_args()
     print(f'args={args}')
 
-    print(f'Image to be processed: {args.input_image}.jpg')
+    print(f'Image to be processed: {args.input}.jpg')
     if(args.mode == 'box' and args.box != None):
         print(f'Box mode chosen; box={args.box}')
         args.box = convert_box_xywh_to_xyxy(ast.literal_eval(args.box))
@@ -34,6 +36,7 @@ def get_arguments():
         parser.error('Points mode requires the --points argument')
     elif(args.mode == 'points' and args.point_labels == None):
         parser.error('Points mode requires the --point_labels argument')
+    
     return args
 
 def main():
@@ -41,11 +44,11 @@ def main():
     args = get_arguments()
 
     model = FastSAM('./weights/FastSAM-x.pt')
-    image_path = f'{args.working_directory}/{args.manufacturer}/Input/{args.input_image}.jpg'
-    output_path = f'{args.working_directory}/{args.manufacturer}/Output/{args.target}/{args.mode}'
-    DEVICE = 'CUDA'
-    everything_results = model(image_path, device=DEVICE, retina_masks=True, imgsz=1024, conf=0.4, iou=0.9,)
-    prompt_process = FastSAMPrompt(image_path, everything_results, device=DEVICE)
+    image_path = f'{args.working_directory}/{args.manufacturer}/Input/{args.input}.jpg'
+    output_path = f'{args.working_directory}/{args.manufacturer}/Outputs/{args.target}/FastSAM results/{args.mode}/'
+    # DEVICE = 'CUDA'
+    everything_results = model(image_path, device=args.device, retina_masks=True, imgsz=1024, conf=0.4, iou=0.9,)
+    prompt_process = FastSAMPrompt(image_path, everything_results, device=args.device)
 
     # everything prompt
     # ann = prompt_process.everything_prompt()
@@ -69,12 +72,12 @@ def main():
             mask = mask['segmentation']
         os.makedirs(f'{output_path}/Masks/', exist_ok=True)
         print(f"saving annotations mask {i} to folder {output_path}/Masks/")
-        plt.imsave(f'{output_path}/Masks/{args.input_image}.png', mask)
+        plt.imsave(f'{output_path}/Masks/{args.input}.png', mask)
 
     #save reference image
     os.makedirs(f'{output_path}/Images/', exist_ok=True)
-    print(f"Promting FastSAM for ")
-    prompt_process.plot(annotations=ann, output_path=f'{output_path}/Images/{args.input_image}.jpg',withContours=True)
+    print(f"Promting FastSAM for {args.input} using {args.mode}-promt")
+    prompt_process.plot(annotations=ann, output_path=f'{output_path}/Images/{args.input}.jpg',withContours=True)
 
 if __name__ == "__main__":
     main()
